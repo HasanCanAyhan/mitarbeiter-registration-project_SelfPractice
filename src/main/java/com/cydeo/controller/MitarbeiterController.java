2d43bac03888a8c9073baca1eb36ef5181ebf98e
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-28 10:56:06 +0200

    mitarbeiter-registration-project

--- src/main/java/com/cydeo/controller/MitarbeiterController.java
+++ src/main/java/com/cydeo/controller/MitarbeiterController.java
@@ -1,6 +1,9 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Mitarbeiter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MitarbeiterController {
 
     @GetMapping("/register")
-    public String createMitarbeiter(){
+    public String createMitarbeiter(Model model){
 
+        model.addAttribute("mitarbeiter",new Mitarbeiter());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
 
         return "mitarbeiter/mitarbeiter-create";
 
